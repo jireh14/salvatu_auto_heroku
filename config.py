f89737f6b3622d94198f2996c4d8901b2ca1c3a6
Jireh Hernandez Castillo <1717110620@utectulancingo.edu.mx>
2019-04-02 23:50:07 -0600

    Base de Datos REMOTA LISTA

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import web
-
+'''
 db_host = 'localhost'
 db_name = 'salva'
 db_user = 'salva'
 db_pw = 'salva.2019'
+'''
+db_host = 'bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'sg7ffsy17wro9gze'
+db_user = 'pbcuzizlwoprp2rq'
+db_pw = 'kmxqle54xdy723d4'
 
 db = web.database(
     dbn='mysql',
